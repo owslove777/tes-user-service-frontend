6f0e9de0b58a4591c9332e257ef354e8f0eb0f9d
Jaden <owslove777@name.com>
2022-07-08 01:29:46 +0900

    modify condition

--- src/components/my_page/MyPage.jsx
+++ src/components/my_page/MyPage.jsx
@@ -14,15 +14,17 @@ const MyPage = () => {
   console.log(userInfo);
 
   let showActivity = [];
+  let showLink = null;
 
-  if (userInfo.userType == "seller"){
+  if (userInfo.userType == "seller") {
     showActivity[0] = <p>마지막 업데이트 일시 : {myPageInfo.lastServiceDate} </p>
     showActivity[1] = <p>받은요청 종료 건수 : {myPageInfo.userRequestCntDone} </p>
-    showActivity[2] = <p>총 받은요청 건수 : {myPageInfo.userRequestCntTotal} </p>    
+    showActivity[2] = <p>총 받은요청 건수 : {myPageInfo.userRequestCntTotal} </p>
     showActivity[3] = <p>총 별점 개수 : {myPageInfo.myServiceCnt} </p>
     showActivity[4] = <p>총 별점 평균 : {myPageInfo.myServiceRate} </p>
-    
-  } else if (userInfo.userType == "user"){
+    showLink = <Link to="/talentRegister">재능등록내역</Link>
+
+  } else if (userInfo.userType == "user") {
     showActivity[0] = <p>마지막 업데이트 일시 : {myPageInfo.lastServiceDate} </p>
     showActivity[1] = <p>요청의뢰 종료 건수 : {myPageInfo.myRequestCntDone}</p>
     showActivity[2] = <p>총 요청의뢰 건수 : {myPageInfo.myRequestCntTotal} </p>
@@ -50,16 +52,16 @@ const MyPage = () => {
         <div className={styles.imageArea}>
           <img className={styles.imageProfile} src={userInfo.imageUrl}></img>
           <div className={styles.useInfo}>
-          <p>{userInfo.name} </p>
-          <p>{userInfo.id} </p>
-          <p>{userInfo.userType} </p>
+            <p>{userInfo.name} </p>
+            <p>{userInfo.id} </p>
+            <p>{userInfo.userType} </p>
           </div>
         </div>
         <div className={styles.infoArea}>
           <h4>활동 요약</h4>
-            {showActivity}
+          {showActivity}
           <h4>활동 내역</h4>
-          <Link to="/talentRegister">재능등록내역</Link>
+          {showLink}
           <Link to="/contractList">계약내역</Link>
           <Link to="/starRatingList">별점내역</Link>
           <h4>설정</h4>
